253d3e629c06ca67ce884785a89387ea666a1606
kanbara-ap <s.kanbara@alphapolis.co.jp>
2022-05-02 10:19:17 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by WEBSYSTEM-MAC31 on 2022/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
